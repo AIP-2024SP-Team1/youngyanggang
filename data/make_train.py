72964cbfc59c7c1a123940586d9437de76150827
fine-pine <lsm3645@g.skku.edu>
2024-05-30 18:11:28 +0900

    feat: make train df json

---
+++ data/make_train.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import json
+
+file_path = './ftqa_wh_2_train1.xlsx'
+df = pd.read_excel(file_path)
+
+data_dict = {}
+id=0
+
+for _, row in df.iterrows():
+    unique_key = (row['book_id'], row['section_id'])
+    cor_section = row['cor_section']
+    question = row['question']
+
+    if unique_key in data_dict:
+        data_dict[unique_key]['output'].append(question)
+    else:
+        data_dict[unique_key] = {
+            "id": id,
+            "instruction": "Generate appropriate multiple questions considering the context of the input. The interrogative word of the question should be one of the following seven: [what, when, where, which, who, why, how].",
+            "input": cor_section,
+            "output": [question],
+            "book_name": row['book_name'],
+            "section_id": row['section_id']
+        }
+        id+=1
