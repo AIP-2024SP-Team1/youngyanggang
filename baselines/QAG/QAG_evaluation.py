1b5ad6049ce2d47f5e9fbf1de6243c8e301f3344
Dongbeen Kim <80032329+EastShineK@users.noreply.github.com>
2024-06-02 07:01:16 +0900

    Write indeed evaluation code

--- baselines/QAG/QAG_evaluation.py
+++ baselines/QAG/QAG_evaluation.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 import re
 import sys
+import ast
 from evaluate import load
 from ignite.metrics import Bleu
 from torchmetrics.text.rouge import ROUGEScore
@@ -81,3 +82,38 @@ def bleurt_eval(df):
     score = sum(results) / len(results)        
     print('BLEURT:', score)
     return score
+
+if __name__ == "__main__":
+    TYPE2CONTROL_SIGNAL = ['What', 'How', 'Who', 'Why', 'Where', 'When', 'Which']
+
+    # path for generated result
+    result_dir = 'output/pred_q.csv'
+    result_df = pd.read_csv(result_dir)
+    for i in TYPE2CONTROL_SIGNAL:
+        result_df[i] = result_df[i].apply(lambda x: ast.literal_eval(x))
+        
+    # path for golden data
+    test_dir = 'data/ftqa_wh_2_test1.csv'
+    gold_df = pd.read_csv(test_dir)
+    gold_df = gold_df.loc[:,['cor_section', 'question']]
+    gold_df.columns = ['context', 'question']
+
+    # merge dataframe
+    gb = gold_df.groupby(['context'])
+    result = pd.DataFrame(gb['question'].unique())
+
+    tot_df = pd.merge(result, result_df, on="context")
+    tot_df = tot_df.dropna()
+    tot_df['tot_gen'] = tot_df.apply(lambda x: concat_gen(x, 4, TYPE2CONTROL_SIGNAL), axis=1)
+
+    #rougel
+    rougel_eval(tot_df)
+
+    #selfbleu
+    selfbleu_eval(tot_df)
+
+    #bertscore
+    bertscore_eval(tot_df)
+
+    #bleurt
+    bleurt_eval(tot_df)
