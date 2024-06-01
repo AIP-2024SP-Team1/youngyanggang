79b9fabb0132c28c9cc20d66bb3b96c14abac27c
fine-pine <lsm3645@g.skku.edu>
2024-06-01 14:03:52 +0900

    feat: scoring BERTScore, BLEURT, Self-BLEU

--- multimodal7b/score.py
+++ multimodal7b/score.py
@@ -5,6 +5,9 @@ import ast
 scores = pd.read_csv('./output/merged(before).csv')
 
 rouge_l = scores['Rouge-L']
+bert_scores = scores['BERTScore']
+bleurt = scores['BLEURT']
+self_bleu = scores['Self-BLEU']
 
 sum = 0
 for r in rouge_l: 
@@ -12,3 +15,20 @@ for r in rouge_l:
     m = max(r)
     sum += m
 print('rouge-L:', sum/len(rouge_l))
+
+sum = 0
+for b in bert_scores:
+    b = ast.literal_eval(b)
+    m = max(b)
+    sum += m
+print('BERTScore:', sum/len(rouge_l))
+
+sum = 0
+for b in bleurt:
+    b = ast.literal_eval(b)
+    m = max(b)
+    sum += m
+print('BLEURT:', sum/len(rouge_l))
+
+s = np.mean(list(np.mean(ast.literal_eval(item)) for item in self_bleu))
+print('Self-BLEU:', s)
