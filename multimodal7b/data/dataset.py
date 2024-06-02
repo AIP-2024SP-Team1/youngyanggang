981451e4dec0be5938d10626fecaafd837208a70
fine-pine <lsm3645@g.skku.edu>
2024-06-02 22:30:35 +0900

    feat: choose the best format prompt

--- multimodal7b/data/dataset.py
+++ multimodal7b/data/dataset.py
@@ -20,13 +20,13 @@ except ImportError:
 
 PROMPT_DICT = {
     "prompt_input": (
-        "An instruction for a task is presented below, paired with contextual input. "
-        "Write a response that appropriately meets the request."
+        "Below is an instruction that describes a task, paired with an input that provides further context. "
+        "Write a response that appropriately completes the request.\n\n"
         "### Instruction:\n{instruction}\n\n### Input:\n{input}\n\n### Response:"
     ),
     "prompt_no_input": (
-        "An instruction for a task is presented below. "
-        "Write a response that appropriately meets the request."
+        "Below is an instruction that describes a task. "
+        "Write a response that appropriately completes the request.\n\n"
         "### Instruction:\n{instruction}\n\n### Response:"
     ),
 }
