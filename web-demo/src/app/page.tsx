3d8fa85fe33cc046243c0c4cb51b099d027a4da1
fine-pine <lsm3645@g.skku.edu>
2024-06-03 22:23:54 +0900

    feat: add context click handlers

--- web-demo/src/app/page.tsx
+++ web-demo/src/app/page.tsx
@@ -3,7 +3,7 @@
 import Header from "./_component/Header";
 import Output from "./_component/Output";
 
-import { ArrowPathIcon } from "@heroicons/react/24/solid";
+import { ArrowRightCircleIcon, TrashIcon } from "@heroicons/react/24/outline";
 import { OutputType } from "./_lib";
 import { useState } from "react";
 
@@ -67,14 +67,41 @@ const outputs_sample: OutputType[] = [
 ];
 
 export default function Home() {
+  const [id, setId] = useState(8); // id starts from 8
   const [outputs, setOutputs] = useState<OutputType[]>(outputs_sample);
+
+  const onSubmit = () => {
+    setOutputs((prev) => [
+      ...prev,
+      {
+        id,
+        question: "What is the flax in full bloom?",
+        answer:
+          "The flax in full bloom is a plant with pretty little blue flowers, described as delicate as the wings of a moth. It is thriving under the sun and showers, which make it look even more beautiful and contribute to its growth, making it fine and long, ideal for producing a beautiful piece of linen. The flax is expressing its happiness and fortune due to its flourishing condition and the potential it holds for becoming something valuable.",
+        upvotes: 0,
+        downvotes: 0,
+      },
+    ]);
+    setId((prev) => prev + 1);
+  };
+
+  const onClear = () => {
+    if (!confirm("Are you sure you want to clear all questions?")) return;
+    setOutputs([]);
+  };
 
   return (
     <>
       <section className="fixed min-w-2c w-[25%] max-w-4c h-dvh p-8 flex flex-col gap-16 bg-white">
         <div className="flex items-center justify-between">
           <h2 className="font-bold">context</h2>
-          <ArrowPathIcon className="w-4 h-4 cursor-pointer" />
+          <div className="flex items-center gap-4">
+            <ArrowRightCircleIcon
+              className="w-5 h-5 cursor-pointer"
+              onClick={onSubmit}
+            />
+            <TrashIcon className="w-4 h-4 cursor-pointer" onClick={onClear} />
+          </div>
         </div>
         <textarea
           className="bg-gray-50 focus:outline-none"
